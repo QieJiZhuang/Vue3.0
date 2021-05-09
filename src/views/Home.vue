93522adebe4abdc6fb62b21e1cd229f6c91b9197
qiejizhuang <15950792990@163.com>
2021-05-10 00:19:21 +0800

    vue全家桶+ts

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+		<h1>home</h1>
+	</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+    components: {
+       
+    },
+})
+
+//extends  延展
+export default class Home extends Vue {
+
+
+}
+</script>
